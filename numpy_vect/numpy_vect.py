7d1f40bec127c7b8d31f699b90c3f81c755bb0c6
Romain Thibault <romain.thibault@mclaren.com>
2019-09-10 12:33:20 +0100

    added more examples

--- numpy_vect/numpy_vect.py
+++ numpy_vect/numpy_vect.py
@@ -28,8 +28,10 @@ try:
 	## THE MAIN PROGRAM STARTS HERE
 	## -------------------------------------------------------	
 
+	import pandas as pd
 	import numpy as np
 	import time
+	import random
 	
 	def get_area(a, b, c):
 		
@@ -39,10 +41,24 @@ try:
 			return a*b/(1000**2)
 		else:
 			raise Exception("Invalid c value = '{}'".format(c))
-			
+		
+	def get_area_np_where(a, b, c):
+		return np.where(c == "m2", a*b, a*b/(1000**2))
+	
 	def get_formatted_area(a, b, c):
-
 		return "{} {}".format(get_area(a, b, c), c)	
+	
+	def get_area_m2(a, b):
+		return a*b
+
+	def get_area_km2(a, b):
+		return a*b/(1000**2)
+		
+	def get_formatted_area_m2(a, b):
+		return "{} m2".format(get_area_m2(a, b))
+	
+	def get_formatted_area_km2(a, b):
+		return "{} km2".format(get_area_km2(a, b))
 		
 	def int_to_unit(ii):
 	
@@ -57,27 +73,79 @@ try:
 	print("-------------------")
 	print()
 	print("get_area : Compute area from two lengths (a, b) and select unit from an input string (c)")
-	print("This function works as standalone (with integers and strings) and with numpy arrays when converted via numpy.vectorize")
 	print()
-	
-	a = 250
-	b = 650
-	c = "m2"
-	print("{}*{}; c = {}: ".format(a, b, c), get_formatted_area(a, b, c))
-	print()
-	
-	# numpy vectorize functions
-	v_get_formatted_area = np.vectorize(get_formatted_area)
-	v_int_to_unit = np.vectorize(int_to_unit)
 	
 	# numpy array size
-	ss = 1000000
-	print("numpy array size: {}".format(ss))
+	ss = 5000000
+	print("array size: {}".format(ss))
+	print()
+	
+	# list of parameters
+	a_list = [1000*random.random() for i in range(ss)]
+	b_list = [1000*random.random() for i in range(ss)]
+	c_list = [int_to_unit(int(2*random.random())) for i in range(ss)]
+	
+	# numpy vectorize functions
+	v_get_area = np.vectorize(get_area)
+	
+	# list to numpy array
+	a_npa = np.array(a_list)
+	b_npa = np.array(b_list)
+	c_npa = np.array(c_list)
+	
+	# lists to pandas dataframe
+	abc_df = pd.DataFrame.from_dict({'a':a_list, 'b':b_list, 'c':c_list})
+	
+	print("pandas (scalar/array function)")
+	print("------------------------------")
+	start_time = time.time()
+	abc_df['rr'] = get_area_np_where(abc_df['a'], abc_df['b'], abc_df['c'])
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	print()
+	
+	print("pandas (np.where)")
+	print("-----------------")
+	start_time = time.time()
+	abc_df['rr2'] = np.where(abc_df.c == "m2", abc_df.a*abc_df.b, abc_df.a*abc_df.b/(1000**2))
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	print()
+	
+	print("pandas (np.select)")
+	print("-----------------")
+	start_time = time.time()
+	abc_df['rr3'] = np.select(condlist = [abc_df.c == "m2", abc_df.c == "km2"], choicelist = [abc_df.a*abc_df.b, abc_df.a*abc_df.b/(1000**2)], default = -1)
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	print()
+	# import pdb; pdb.set_trace()
+	
+	print("numpy")
+	print("-----")
+	start_time = time.time()
+	m2_indexes = (c_npa == "m2")
+	km2_indexes = (c_npa == "km2")
+	rr_m2_npa = get_area_m2(a_npa[m2_indexes], b_npa[m2_indexes])
+	rr_km2_npa = get_area_km2(a_npa[km2_indexes], b_npa[km2_indexes])
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	print()
+	
+	print("For-loop")
+	print("--------")
+	start_time = time.time()
+	rr_list = []
+	for index_ in range(ss):
+		rr_list.append(get_area(a_list[index_], b_list[index_], c_list[index_]))
+	# print(rr_list)
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	print()
+	
+	print("np.vectorize")
+	print("-------------")
 	
 	# compute results
 	start_time = time.time()
-	print(v_get_formatted_area(np.random.randint(0, 1000, size = ss), np.random.randint(0, 1000, size = ss), v_int_to_unit(np.random.randint(0, 2, size = ss))))
-	print("Time elapsed: {:.2} seconds".format(time.time() - start_time))
+	rr_npa = v_get_area(a_npa, b_npa, c_npa)
+	print("Time elapsed: {:.3f} seconds".format(time.time() - start_time))
+	
 
 ## -------- SOMETHING WENT WRONG -----------------------------	
 except:
